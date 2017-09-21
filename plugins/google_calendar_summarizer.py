8f961ed816e8b8b47bf0a99aa6e17ace97961235
remvst <remvst@gmail.com>
2017-09-21 01:08:44 -0400

    Moved get_credential to google summarizer file

--- plugins/google_calendar_summarizer.py
+++ plugins/google_calendar_summarizer.py
@@ -1,10 +1,36 @@
 import httplib2
+import os
 from datetime import date, datetime, time, timedelta
 from dateutil import parser
 
 from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
 
 from lib.text_plugin import TextPlugin
+
+def get_credentials(client_secrets_file, application_name):
+    """
+    https://developers.google.com/google-apps/calendar/quickstart/python
+    """
+    SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
+
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+
+    credential_path = os.path.join(credential_dir, 'home-ai-google-calendar.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(client_secrets_file, SCOPES)
+        flow.user_agent = application_name
+        credentials = tools.run_flow(flow, store)
+
+    return credentials
 
 class GoogleCalendarSummarizer(TextPlugin):
 
