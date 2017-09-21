50e024d3e9210bdf9eb1e809bcd2d7f0fb4b0846
remvst <remvst@gmail.com>
2017-09-21 01:29:53 -0400

    Added KikBot output type

---
+++ output/kik_bot.py
@@ -0,0 +1,21 @@
+from kik import KikApi
+from kik.messages import TextMessage
+
+from output import Output
+
+
+class KikBot(Output):
+
+    def __init__(self, bot_username, bot_api_key, recipient_username):
+        super(KikBot, self).__init__()
+
+        self.kik = KikApi(bot_username, bot_api_key)
+        self.recipient_username = recipient_username
+
+    def output(self, string):
+        self.kik.send_messages([
+            TextMessage(
+                to=self.recipient_username,
+                body=string
+            )
+        ])
