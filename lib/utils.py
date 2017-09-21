c365cc77827aff0918d9d5db29be5ead22e1a9e1
remvst <remvst@gmail.com>
2017-09-20 20:54:35 -0400

    Early framework to talk and check if user is in network

---
+++ lib/utils.py
@@ -0,0 +1,26 @@
+import os
+import subprocess
+from gtts import gTTS
+from playsound import playsound
+
+def is_user_in_network(mac_address, timeout=5000):
+    arp_scan = subprocess.check_output([
+        'arp-scan',
+        '-l',
+        '--timeout={}'.format(timeout)
+    ])
+    return mac_address in arp_scan
+
+def say(text):
+    file_path='speech.mp3'
+
+    try:
+        os.remove(file_path)
+    except OSError:
+        pass # file doesn't exist, no need to freak out
+
+    tts = gTTS(text=text, lang='en')
+    tts.save(file_path)
+    playsound(file_path)
+
+    os.remove(file_path)
