dedd553af1619f1222c732802e3d3cf41e05d552
remvst <remvst@gmail.com>
2017-10-10 00:05:29 -0400

    Added ngrok mess

--- lib/utils.py
+++ lib/utils.py
@@ -1,3 +1,5 @@
+import httplib2
+import json
 import subprocess
 
 def is_user_in_network(mac_address, timeout=5000):
@@ -7,3 +9,18 @@ def is_user_in_network(mac_address, timeout=5000):
         '--timeout={}'.format(timeout)
     ])
     return mac_address in arp_scan
+
+def get_ngrok_url():
+    tunnels_url='http://localhost:4040/api/tunnels'
+
+    http = httplib2.Http()
+    resp, content = http.request(tunnels_url)
+
+    assert resp.status == 200
+
+    json_response = json.loads(content)
+
+    return json_response['tunnels'][0]['public_url']:
+
+def run_ngrok(port):
+    subprocess.check_output(['./ngrok', 'http', str(port)])
