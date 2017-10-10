251e32a31458834c0a59bc8d06b720f2445a49c9
remvst <remvst@gmail.com>
2017-10-10 00:43:41 -0400

    Early receiving messages

--- output/kik_bot.py
+++ output/kik_bot.py
@@ -1,5 +1,9 @@
-from kik import KikApi
-from kik.messages import TextMessage
+import json
+
+from flask import Flask, request
+from kik import KikApi, Configuration
+from kik.messages import TextMessage, messages_from_json
+from werkzeug.serving import run_simple
 
 from output import Output
 
@@ -12,6 +16,10 @@ class KikBot(Output):
         self.kik = KikApi(bot_username, bot_api_key)
         self.recipient_username = recipient_username
 
+        self.config = Configuration(webhook=None)
+
+        self.flask_app = Flask(__name__)
+
     def output(self, string):
         try:
             self.kik.send_messages([
@@ -22,3 +30,26 @@ class KikBot(Output):
             ])
         except Exception as e:
             print e
+
+    def update_config(self, webhook):
+        self.config.webhook = webhook
+        self.kik.set_configuration(self.config)
+
+    def http_worker(self, port):
+        # Add the route the dirty way
+        self.flask_app.route('/', methods=['POST'])(self.incoming_messages)
+
+        # And run the server!
+        run_simple('localhost', port, self.flask_app)
+
+    def incoming_messages(self):
+        signature = request.headers.get('X-Kik-Signature')
+        raw_body = request.get_data()
+
+        if not self.kik.verify_signature(signature, raw_body):
+            return '', 403
+
+        json_body = json.loads(raw_body)
+        messages = messages_from_json(json_body['messages'])
+
+        return '', 200
