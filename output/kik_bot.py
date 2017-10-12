48e8958eb119ecbe434e635706be4b481b52c9f3
remvst <remvst@gmail.com>
2017-10-11 23:32:33 -0400

    Allowed Kik bot output to server pictures

--- output/kik_bot.py
+++ output/kik_bot.py
@@ -1,9 +1,10 @@
 import json
 import logging
+import os
 
-from flask import Flask, request
+from flask import Flask, request, send_from_directory, url_for
 from kik import KikApi, Configuration
-from kik.messages import TextMessage, messages_from_json
+from kik.messages import PictureMessage, TextMessage, messages_from_json
 from werkzeug.serving import run_simple
 
 from output import Output
@@ -11,37 +12,51 @@ from output import Output
 
 class KikBot(Output):
 
-    def __init__(self, bot_username, bot_api_key, recipient_username, message_handler=None, default_keyboard=None):
+    def __init__(self, bot_username, bot_api_key, recipient_username, message_handler=None, default_keyboard=None, static_folder=None):
         super(KikBot, self).__init__()
 
         self.kik = KikApi(bot_username, bot_api_key)
         self.recipient_username = recipient_username
         self.message_handler = message_handler
         self.default_keyboard = default_keyboard
+        self.static_folder = static_folder
 
         self.config = Configuration(webhook=None)
 
-        self.flask_app = Flask(__name__)
+        self.flask_app = Flask(__name__, static_url_path='/static', static_folder=static_folder)
 
-    def output(self, string):
+    def send(self, messages):
+        for message in messages:
+            message.keyboards = [self.default_keyboard]
+
         try:
-            self.kik.send_messages([
-                TextMessage(
-                    to=self.recipient_username,
-                    body=string,
-                    keyboards=[self.default_keyboard]
-                )
-            ])
+            self.kik.send_messages(messages)
         except Exception as e:
             logging.exception(e)
+
+    def output(self, string):
+        self.send([
+            TextMessage(
+                to=self.recipient_username,
+                body=string
+            )
+        ])
+
+    def output_picture(self, url):
+        self.send([
+            PictureMessage(
+                to=self.recipient_username,
+                pic_url=url
+            )
+        ])
 
     def update_config(self, webhook):
         self.config.webhook = webhook
         self.kik.set_configuration(self.config)
 
     def http_worker(self, port):
-        # Add the route the dirty way
-        self.flask_app.route('/', methods=['POST'])(self.incoming_messages)
+        # Add the routes
+        self.flask_app.add_url_rule(rule='/', methods=['POST'], view_func=self.incoming_messages)
 
         # And run the server!
         run_simple('localhost', port, self.flask_app)
@@ -62,3 +77,7 @@ class KikBot(Output):
                 self.message_handler(message)
 
         return '', 200
+
+    def path_to_static_file(self, relative_path):
+        path_from_static = os.path.relpath(relative_path, self.static_folder)
+        return 'static/{}'.format(path_from_static)
