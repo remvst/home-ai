4ab340c295625e2f8ae671bf6ca2303bdd3086f6
remvst <remvst@gmail.com>
2017-10-13 18:53:32 -0400

    Fixed quiet flag

--- utils/sound.py
+++ utils/sound.py
@@ -7,7 +7,7 @@ def pair_speaker(mac_address, sink_name):
     subprocess.check_call(['./sh/pair_speaker', mac_address, sink_name])
 
 def play_mp3(file_path):
-    subprocess.check_output(['mpg123', file_path, '--quiet'])
+    subprocess.check_output(['mpg123', '--quiet', file_path])
 
 def text_to_speech(string, file_path='speech.mp3'):
     tts = gTTS(text=string, lang='en')
