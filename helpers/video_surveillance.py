42e7f86efa4c09c446462a60fa24ba4883d21d8c
remvst <remvst@gmail.com>
2017-10-13 19:50:47 -0400

    Fixed result

--- helpers/video_surveillance.py
+++ helpers/video_surveillance.py
@@ -42,7 +42,7 @@ class VideoSurveillance(object):
         hog = cv2.HOGDescriptor()
         hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
-        found, humans = hog.detectMultiScale(picture, winStride=(8, 8), padding=(32, 32), scale=1.05)
+        humans, weights = hog.detectMultiScale(picture, winStride=(4, 4), padding=(8, 8), scale=1.05)
 
         for (x, y, w, h) in humans:
             cv2.rectangle(enhanced, (x, y), (x + w, y + h), (0, 0, 255), 2)
