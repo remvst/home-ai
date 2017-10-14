22da20d744f01e52e90360cc5a9632ed87a3c8a3
remvst <remvst@gmail.com>
2017-10-13 22:58:25 -0400

    Capturing more frames

--- utils/camera.py
+++ utils/camera.py
@@ -5,7 +5,7 @@ def take_picture(destination, skip=1, resolution=(1280, 720)):
         'fswebcam',
         '-r', '{}x{}'.format(resolution[0], resolution[1]),
         '--skip', '1',
-        '--frames', '1',
+        '--frames', '10',
         '--no-banner',
         '--quiet',
         destination
