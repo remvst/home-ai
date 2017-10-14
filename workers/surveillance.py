53523046472b079d8881e8693cdad664165b2da0
remvst <remvst@gmail.com>
2017-10-13 23:19:03 -0400

    Generate speech before picture

--- workers/surveillance.py
+++ workers/surveillance.py
@@ -14,11 +14,11 @@ from workers.speech import add_to_queue
 def face_detected(picture_path, enhanced_path):
     logging.info('Face detected')
 
-    url = '{}/{}'.format(get_ngrok_url(), path_to_static_file(enhanced_path))
-    bot.send([PictureMessage(pic_url=url, to=config.KIK_BOT_RECIPIENT_USERNAME)])
-
     string = generate_string()
     add_to_queue(string)
+
+    url = '{}/{}'.format(get_ngrok_url(), path_to_static_file(enhanced_path))
+    bot.send([PictureMessage(pic_url=url, to=config.KIK_BOT_RECIPIENT_USERNAME)])
 
 surveillance = VideoSurveillance(pictures_folder='{}/surveillance'.format(app.static_folder))
 surveillance.detection_handler = face_detected
