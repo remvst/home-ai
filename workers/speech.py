e7d5354b8526a8fef0f90210e18898cf4897b3d6
remvst <remvst@gmail.com>
2017-10-14 11:29:58 -0400

    Maybe improve speech

--- workers/speech.py
+++ workers/speech.py
@@ -1,6 +1,7 @@
 import os
 import threading
 from time import sleep
+from uuid import uuid4
 
 import config
 from utils.sound import play_mp3, text_to_speech, pair_speaker
@@ -10,8 +11,13 @@ queue = []
 lock = threading.Lock()
 
 def add_to_queue(string):
+    file_path = 'static/speech/{}.mp3'.format(str(uuid4))
+    text_to_speech(string=string, file_path=file_path)
+
+    profile_cascade = cv2.CascadeClassifier('assets/haarcascade_profileface.xml')
+
     lock.acquire()
-    queue.append(string)
+    queue.append(file_path)
     lock.release()
 
 def worker():
@@ -21,21 +27,13 @@ def worker():
         try:
             # Play first item in the queue
             if len(queue) > 0:
-                play_string(queue.pop(0))
+                play_speech(queue.pop(0))
         finally:
             lock.release()
 
         sleep(0.5)
 
-def play_string(string):
-    file_path = 'speech.mp3'
-
-    try:
-        os.remove(file_path)
-    except OSError:
-        pass # file doesn't exist, no need to freak out
-
-    text_to_speech(string=string, file_path=file_path)
+def play_speech(file_path):
     pair_speaker(mac_address=config.SPEAKER_MAC_ADDRESS, sink_name=config.SINK_NAME)
 
     play_mp3('assets/speech-announcement.mp3')
