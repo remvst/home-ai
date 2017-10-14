7ea0d8748cc830a5f121f70a65de84729fdd1076
remvst <remvst@gmail.com>
2017-10-13 23:07:59 -0400

    Maybe fix rsstart

--- utils/process.py
+++ utils/process.py
@@ -3,9 +3,5 @@ import psutil
 import sys
 
 def restart_process():
-    p = psutil.Process(os.getpid())
-    for handler in p.get_open_files() + p.connections():
-        os.close(handler.fd)
-
     python = sys.executable
     os.execl(python, python, *sys.argv)
