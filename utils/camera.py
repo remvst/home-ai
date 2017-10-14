9f4ce96e73b66b02c396782fd9d9ea262930c2b6
remvst <remvst@gmail.com>
2017-10-14 11:25:25 -0400

    Hopefully improving capture quality

--- utils/camera.py
+++ utils/camera.py
@@ -1,13 +1,13 @@
 import logging
 import subprocess
 
-def take_picture(destination, skip=1, resolution=(1280, 720)):
+def take_picture(destination, skip=2, resolution=(1280, 720)):
     logging.debug('Taking picture')
     subprocess.check_output([
         'fswebcam',
         '-r', '{}x{}'.format(resolution[0], resolution[1]),
         '--skip', str(skip),
-        '--frames', '5',
+        '--frames', '2',
         '--no-banner',
         '--quiet',
         destination
