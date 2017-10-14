7009086ecb6329ede8e142ac8c671d2c86345aa7
remvst <remvst@gmail.com>
2017-10-14 11:09:37 -0400

    Added log

--- utils/camera.py
+++ utils/camera.py
@@ -1,6 +1,8 @@
+import logging
 import subprocess
 
 def take_picture(destination, skip=1, resolution=(1280, 720)):
+    logging.debug('Taking picture')
     subprocess.check_output([
         'fswebcam',
         '-r', '{}x{}'.format(resolution[0], resolution[1]),
