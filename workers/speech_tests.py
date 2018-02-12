8605df5e116325f01db153fe51861f7f25f0952b
remvst <remvst@gmail.com>
2018-02-12 00:27:23 -0500

    More things

--- workers/speech_tests.py
+++ workers/speech_tests.py
@@ -3,7 +3,7 @@ from pocketsphinx import LiveSpeech
 from bot.kik_bot import bot
 
 
-speech = LiveSpeech(sampling_rate=16000, lm='9891.lm', dic='9891.dic')
+speech = LiveSpeech(sampling_rate=16000, lm='9891.lm', dic='9891.dic', audio_device='0')
 
 def worker():
     for phrase in speech:
