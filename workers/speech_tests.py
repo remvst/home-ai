a316c66873855e90f107ebf06ae89d7e9db3ee2a
remvst <remvst@gmail.com>
2018-02-12 00:51:33 -0500

    Added things

--- workers/speech_tests.py
+++ workers/speech_tests.py
@@ -3,7 +3,9 @@ from pocketsphinx import LiveSpeech
 from bot.kik_bot import bot
 from workers.speech import add_to_queue
 
+print 'Initializing speech things'
 speech = LiveSpeech(sampling_rate=16000, lm='assets/dictionary/8586.lm', dic='assets/dictionary/8586.dic', audio_device='0')
+print 'Initialized speech things'
 
 def worker():
     for phrase in speech:
