8d56d4cead99a9d6fa70fcaa933c77bf2101e012
remvst <remvst@gmail.com>
2018-02-12 01:13:59 -0500

    Logging

--- workers/speech_tests.py
+++ workers/speech_tests.py
@@ -1,19 +1,21 @@
-print 'Initializing speech things maybe'
+import logging
+
+logging.debug('Initializing speech things maybe')
 
 from pocketsphinx import LiveSpeech
 
 from bot.kik_bot import bot
 from workers.speech import add_to_queue
 
-print 'Initializing speech things'
+logging.debug('Initializing speech things')
 speech = LiveSpeech(sampling_rate=16000, lm='assets/dictionary/9126.lm', dic='assets/dictionary/9126.dic', audio_device='0')
-print 'Initialized speech things'
+logging.debug('Initialized speech things')
 
 def worker():
     for phrase in speech:
         text = phrase.hypothesis()
-        print 'GOOOOOOOOOOO'
-        print text
+        logging.debug('GOOOOOOOOOOO')
+        logging.debug(text)
 
         if 'please' not in text:
             continue
