c6080812af5d1261181a715a8e5ee33d26f6ada2
remvst <remvst@gmail.com>
2018-02-11 23:32:56 -0500

    Whoops that should be in a function

--- workers/speech_tests.py
+++ workers/speech_tests.py
@@ -2,13 +2,14 @@ from pocketsphinx import LiveSpeech
 
 from bot.kik_bot import bot
 
-speech = LiveSpeech(sampling_rate=16000)
+def worker():
+    speech = LiveSpeech(sampling_rate=16000)
 
-for phrase in speech:
-    text = phrase.hypothesis()
-    print text
+    for phrase in speech:
+        text = phrase.hypothesis()
+        print text
 
-    if 'please' not in text:
-        continue
+        if 'please' not in text:
+            continue
 
-    bot.handle_text(text)
+        bot.handle_text(text)
