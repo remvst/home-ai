a3aa2621b888e12366f1adc06f5f11a10f88b52d
remvst <remvst@gmail.com>
2018-02-11 23:38:53 -0500

    Main thread yo

--- workers/speech_tests.py
+++ workers/speech_tests.py
@@ -2,9 +2,10 @@ from pocketsphinx import LiveSpeech
 
 from bot.kik_bot import bot
 
-def worker():
-    speech = LiveSpeech(sampling_rate=16000)
 
+speech = LiveSpeech(sampling_rate=16000)
+
+def worker():
     for phrase in speech:
         text = phrase.hypothesis()
         print text
