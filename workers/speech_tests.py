fafb3a58615ad9978dd9dff3897b7facd70f697e
remvst <remvst@gmail.com>
2018-02-12 01:41:11 -0500

    Added sounds

--- workers/speech_tests.py
+++ workers/speech_tests.py
@@ -3,6 +3,7 @@ import logging
 from pocketsphinx import LiveSpeech
 
 from bot.kik_bot import bot
+from utils.sound import play_mp3
 from workers.speech import add_to_queue
 
 speech = LiveSpeech(sampling_rate=16000, lm='assets/dictionary/9126.lm', dic='assets/dictionary/9126.dic', audio_device='0')
@@ -15,5 +16,8 @@ def worker():
         if not text.startswith('please'):
             continue
 
+        play_mp3('assets/speech-detected.mp3')
+
         if not bot.handle_text(text):
+            play_mp3('assets/error.mp3')
             add_to_queue('unrecognized command')
