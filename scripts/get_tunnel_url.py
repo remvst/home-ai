86313ff0d389fa0c08efaf48c24d6a7a45e05725
remvst <remvst@gmail.com>
2018-02-13 00:44:04 -0500

    Ugh remi

--- scripts/get_tunnel_url.py
+++ scripts/get_tunnel_url.py
@@ -6,6 +6,4 @@ from utils.script import Script
 class GetTunnelURLScript(Script):
 
     def run(self, input, output):
-        string = generate_string()
-
         output.output([TextContent(get_ngrok_url())])
