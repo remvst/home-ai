0a8bceacd2f76594ab28cbb5076a208b23a11dc6
remvst <remvst@gmail.com>
2018-02-13 00:54:40 -0500

    Whoops

--- scripts/restart.py
+++ scripts/restart.py
@@ -8,7 +8,7 @@ from utils.script import Script
 class RestartScript(Script):
 
     def __init__(self, *args, **kwargs):
-        super(TakePictureScript, self).__init__(*args, **kwargs)
+        super(RestartScript, self).__init__(*args, **kwargs)
 
     def run(self, input, output):
         output.output([TextContent(body='Restarting in 5 seconds')])
