e2f5db908ef5514cd28c823bfcb57311d4dbac9e
remvst <remvst@gmail.com>
2018-02-15 00:30:15 -0500

    Lol

---
+++ utils/infinite_worker.py
@@ -0,0 +1,19 @@
+import logging
+from threading import Thread
+from time import sleep
+
+def infinite_worker(worker):
+
+    def _worker():
+        while True:
+            try:
+                worker(*args)
+            except KeyboardInterrupt:
+                return
+            except Exception as e:
+                logging.exception(e)
+                sleep(1)
+            finally:
+                pass
+
+    return _worker
