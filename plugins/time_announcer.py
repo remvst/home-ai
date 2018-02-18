f8a69ef0e57f3a9b881407ebfe23d48d86dacfb6
remvst <remvst@gmail.com>
2018-02-17 20:46:55 -0500

    No need for a plugin

--- plugins/time_announcer.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-from plugins.text_plugin import TextPlugin
-
-class TimeAnnouncer(TextPlugin):
-
-    def generate_string(self):
-        formatted_time = datetime.now().strftime('%I:%M%p')
-        return 'It is {}'.format(formatted_time)
--- scripts/current_time.py
+++ scripts/current_time.py
@@ -1,9 +1,5 @@
-import config
-from plugins.google_calendar_summarizer import GoogleCalendarSummarizer, get_credentials
-from plugins.news_summarizer import NewsSummarizer
-from plugins.quote_of_the_day import QuoteOfTheDay
-from plugins.time_announcer import TimeAnnouncer
-from plugins.weather_announcer import WeatherAnnouncer
+from datetime import datetime
+
 from utils.content import TextContent
 from utils.script import Script
 
@@ -11,25 +7,6 @@ from utils.script import Script
 class CurrentTimeScript(Script):
 
     def run(self, input_content):
-        time_announcer = TimeAnnouncer()
 
-        quote_of_the_day = QuoteOfTheDay()
-
-        weather_announcer = WeatherAnnouncer(
-            api_key=config.OPEN_WEATHER_MAP_API_KEY,
-            location_query='waterloo,ontario'
-        )
-
-        lines = [
-            'Good morning Remi. Time to wake up',
-            time_announcer.generate_string(),
-            google_calendar_summarizer.generate_string(),
-            news_summarizer.generate_string(),
-            weather_announcer.generate_string(),
-            quote_of_the_day.generate_string(),
-            'Have an amazing day'
-        ]
-
-        string = u'.'.join(lines)
-
-        self.output([TextContent(strintime_announcer.generate_string())])
+        formatted_time = datetime.now().strftime('%I:%M%p')
+        self.output([TextContent('It is {}'.format(formatted_time))])
